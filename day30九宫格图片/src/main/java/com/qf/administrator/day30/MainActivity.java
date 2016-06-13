0169e965e5fcceeb1324c26129112cf133761961
602366386@qq.com <602366386@qq.com>
2016-06-13 20:53:57 +0800

    添加了一些注释

--- day30九宫格图片/src/main/java/com/qf/administrator/day30/MainActivity.java
+++ day30九宫格图片/src/main/java/com/qf/administrator/day30/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //提交
+        //第二次
     }
 }
